aa7215556303407afa99a473f30552d75773af28
lukito <lukas_civil@hotmail.com>
2019-12-08 14:18:22 -0300

    feat: add sanitized acronym

--- lib/string/acronym.ex
+++ lib/string/acronym.ex
@@ -1,6 +1,6 @@
 defmodule Acronym do
-  @spec abbreviate_simple(String.t) :: String.t
-  def abbreviate_simple(title) do 
+  @spec abbreviate(String.t) :: String.t
+  def abbreviate(title) do 
     separated_names = String.split(title, " ")
     capitalized_letters = capitalized_letters(separated_names)
     Enum.join(capitalized_letters)
@@ -11,6 +11,31 @@ defmodule Acronym do
     Enum.map(names, fn(name)-> 
       first_letter = String.first(name)
       String.capitalize(first_letter)
+    end)
+  end
+
+  # Using pipes and sanitizing names
+
+  @spec abbreviate2([String.t]) :: [String.t]
+  def sanitize(names) do 
+    Enum.filter(names, fn(name)-> 
+      not Enum.member?(["de", "e"], name)
+    end)
+  end
+
+  @spec abbreviate2(String.t) :: String.t
+  def abbreviate2(title) do 
+    String.split(title, " ")
+    |> sanitize()
+    |> capitalized_letters2()
+    |> Enum.join()
+  end
+
+  @spec capitalized_letters([String.t]) :: String.t
+  defp capitalized_letters2(names) do
+    Enum.map(names, fn(name)-> 
+      String.first(name)
+      |> String.capitalize()
     end)
   end
 end
--- test/acronym_test.exs
+++ test/acronym_test.exs
@@ -2,7 +2,11 @@ defmodule AcronymTest do
   use ExUnit.Case
   doctest Acronym
 
-  test "Acronym of 'Instituto Brasileiro de Geografia e Estatística' is 'IBGE'" do
-    assert Acronym.abbreviate_simple("Instituto Brasileiro de Geografia e Estatística") == "IBGE"
+  test "Acronym of 'Instituto Brasileiro de Geografia e Estatística' is 'IBDGEE'" do
+    assert Acronym.abbreviate("Instituto Brasileiro de Geografia e Estatística") == "IBDGEE"
+  end
+
+  test "Acronym of 'Instituto Brasileiro de Geografia e Estatística' is 'IBGE' sanitized" do
+    assert Acronym.abbreviate2("Instituto Brasileiro de Geografia e Estatística") == "IBGE"
   end
 end
